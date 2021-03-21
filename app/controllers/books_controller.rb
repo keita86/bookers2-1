82d2b8aac7ae3042c08f45c52816d0756ee123f9
keita86 <ke4i2ta4@gmail.com>
2021-03-21 06:39:58 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,23 @@
 class BooksController < ApplicationController
 
   def create
-    @book = Book.new(params[:id])
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "book was successfully created"
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render :new
+      render :index
     end
   end
 
   def show
     @book = Book.find(params[:id])
     @books = Book.all
-    @book = Book.new
+    @post = Book.new
   end
+
 
   def index
     @books = Book.all
@@ -36,16 +37,18 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-    
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
+
   def book_params
-  params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_124602) do
+ActiveRecord::Schema.define(version: 2021_03_21_014030) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_id"
+  end
+
+  create_table "userinfos", force: :cascade do |t|
+    t.string "name"
+    t.string "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -29,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_124602) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
