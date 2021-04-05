ff053d86a6810daec981fe1cef6a929764564054
keita86 <ke4i2ta4@gmail.com>
2021-04-05 10:29:31 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  # フォローする
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  # フォロー外す
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+  get "follow_index" => "relationships#follow_index"
+  get "follower_index" => "relationships#follower_index"
+
   root to: 'homes#top'
     get "/home/about" => "homes#about"
   resources :users, only: [:show, :edit, :update, :index]
